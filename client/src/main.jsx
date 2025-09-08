819ded0812196a51bbea5efeb6469067a056a7f7
kuda <kuda2012@gmail.com>
2025-09-08 13:06:12 -0500

    scroll to top

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom"; // <-- add this
-import "./index.css";
 import App from "./App.jsx";
+import ScrollToTop from "./ScrollToTop.jsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </QueryClientProvider>
